09b126c7a68100a51578262f81dc125eb98f72d0
henryc007 <chrishen@oregonstate.edu>
2024-07-19 14:45:56 -0700

    Updated files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application:app
+web: gunicorn -b :80 app:app
--- application.py
+++
@@ -1,3 +0,0 @@
-from app import app  # Assuming `app` is your Flask app instance
-if __name__ == "__main__":
-    app.run()
